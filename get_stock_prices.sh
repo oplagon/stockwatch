0b0da71fbc05e8127987b12fe7551fb024362145
oplagon <oplagon@web.de>
2025-02-19 23:31:20 +0100

    added get_stock_prices.sh and adjusted cron-job.yml to execute

---
+++ get_stock_prices.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# URL der API
+URL="https://api.stockdio.com/data/financial/prices/v1/getlatestprices?app-key=A26E6AECAF394857B017DBB74B100C7C&stockExchange=XETRA&symbols=SIE;ALV;SAP;BAYN;DTE"
+
+# GET Request und Speichern der Antwort in response.txt
+curl -o stock_prices.json $URL
+
+# Überprüfung des Erfolgs des Requests
+if [ $? -eq 0 ]; then
+  echo "Die Antwort wurde erfolgreich in stock_prices.json gespeichert."
+else
+  echo "Fehler beim Abrufen der Daten von der API."
+fi
